e2dc5828b46b159e531738a549ab424e4dbe0a6d
Zach <shaw.zacht@gmail.com>
2022-08-04 13:40:09 -0700

    commit tree 37e8701ce0dcc3d94787ae0f489b172709e30153

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,54 @@
 # your code here
+class Stack
+
+    attr_accessor :stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @limit && stack.length < @limit || @limit == nil
+        @stack.push(value)
+        else 
+            raise 'Stack Overflow'
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        if @stack.length == 0
+            true
+        else false
+        end
+    end
+
+    def full?
+        if @stack.length == @limit
+            true
+        else false
+        end
+    end
+
+    def search(value)
+        value_index = @stack.find_index(value)
+        if value_index
+            @stack.length - 1 - value_index
+        else -1
+        end
+    end
+
+
+end
